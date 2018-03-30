cd3b29de10183c1bc73a1e99d0d684e74c579c3e
zengfeng <zengfeng@shinezone.com>
2018-03-30 14:55:05 +0800

    no message

---
+++ TestUnkonUnityEngeine/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace TestUnkonUnityEngeine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Scene scene = new Scene();
+            GameObject go = new GameObject("Unit");
+            MoveComponent move = go.AddComponent<MoveComponent>();
+            move.MoveTo(new Vector3(100, 0, 0));
+
+            Console.ReadLine();
+
+            //while (true)
+            //{
+            //    try
+            //    {
+            //        Thread.Sleep(1);
+            //        contex.Update();
+            //        Game.EventSystem.Update();
+            //    }
+            //    catch (Exception e)
+            //    {
+            //        Log.Error(e);
+            //    }
+            //}
+        }
+    }
+}
